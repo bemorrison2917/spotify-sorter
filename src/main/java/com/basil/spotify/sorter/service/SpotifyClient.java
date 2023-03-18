bb0cf924dd5c4e7d070f33043a8971b96b397ee4
bemorrison2917 <128191872+bemorrison2917@users.noreply.github.com>
2023-03-18 00:07:37 -0500

    temp

--- src/main/java/com/basil/spotify/sorter/service/SpotifyClient.java
+++ src/main/java/com/basil/spotify/sorter/service/SpotifyClient.java
@@ -1,5 +1,6 @@
 package com.basil.spotify.sorter.service;
 
+import org.slf4j.Logger;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
@@ -20,33 +21,54 @@ private String accessToken;
         this.accessToken = accessToken;
     }
 
-    public <T> T get(String uri, Class<T> responseClass) throws HttpClientErrorException {
-        ResponseEntity<T> response = null;
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
+    public <T> T get(String uri, Class<T> responseClass) throws HttpClientErrorException, InterruptedException {
+      boolean retry = true;
+        ResponseEntity<T> test = null;
+        while(retry) {
+           try {
+               ResponseEntity<T> response = null;
+               RestTemplate restTemplate = new RestTemplate();
+               HttpHeaders headers = new HttpHeaders();
 
-        headers.set("Authorization", "Bearer " + accessToken); //accessToken can be the secret key you generate.
-        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
-        HttpEntity<String> entity = new HttpEntity <> (headers);
+               headers.set("Authorization", "Bearer " + accessToken); //accessToken can be the secret key you generate.
+               headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+               HttpEntity<String> entity = new HttpEntity<>(headers);
 
-        ResponseEntity<T> test = restTemplate.exchange(uri, HttpMethod.GET, entity, responseClass);
-        System.out.println(test.getStatusCode());
+               test = restTemplate.exchange(uri, HttpMethod.GET, entity, responseClass);
+               retry = false;
+           }
+           catch(HttpClientErrorException.TooManyRequests e) {
+               int retryAfter = Integer.parseInt(e.getResponseHeaders().get(HttpHeaders.RETRY_AFTER).get(0));
+               System.out.println("Too many requests, waiting back up period then trying again");
+             //  Thread.sleep(1000 * retryAfter);
+           }
+
+       }
         return test.getBody();
     }
 
-    public <T> T post(String uri, Class<T> responseClass, String requestBody) throws HttpClientErrorException {
-        ResponseEntity<T> response = null;
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
+    public <T> T post(String uri, Class<T> responseClass, String requestBody) throws HttpClientErrorException, InterruptedException {
+        boolean retry = true;
+        ResponseEntity<T> test = null;
+        while(retry) {
+            try {
+                RestTemplate restTemplate = new RestTemplate();
+                HttpHeaders headers = new HttpHeaders();
 
-        //body.add("uris", uris);
+                headers.set("Authorization", "Bearer " + accessToken); //accessToken can be the secret key you generate.
+                headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+                HttpEntity<?> entity = new HttpEntity<>(requestBody, headers);
 
-        headers.set("Authorization", "Bearer " + accessToken); //accessToken can be the secret key you generate.
-        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
-        HttpEntity<?> entity = new HttpEntity <>(requestBody, headers);
-
-        ResponseEntity<T> test = restTemplate.exchange(uri, HttpMethod.POST, entity, responseClass);
-        System.out.println(test.getStatusCode());
+                restTemplate.exchange(uri, HttpMethod.POST, entity, responseClass);
+                System.out.println(test.getStatusCode());
+            }
+            catch(HttpClientErrorException.TooManyRequests e) {
+                int retryAfter = Integer.parseInt(e.getResponseHeaders().get(HttpHeaders.RETRY_AFTER).get(0));
+                System.out.println("Too many requests, waiting back up period then trying again");
+                Thread.sleep(1000 * retryAfter);
+            }
+            retry = false;
+        }
         return test.getBody();
     }
 
